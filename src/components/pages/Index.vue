0f6e80767b0affb3026289cef0ac14e16ec25002
PlayWay <dim.art2011@yandex.ru>
2019-07-06 17:02:40 +0300

    init

---
+++ src/components/pages/Index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    include ../../tools/mixins.pug
+
+    div
+        header-component(@headerHeight="headerHeight")
+        table-component(:height="headHeight")
+
+</template>
+
+<script>
+    import Header from '../pages/content/Header.vue';
+    import Table from '../pages/content/Table.vue';
+
+    export default {
+        data() {
+            return {
+                headHeight: null
+            }
+        },
+        methods: {
+            headerHeight(data) {
+                this.headHeight = data;
+            }
+        },
+        mounted() {
+
+        },
+        destroyed() {
+
+        },
+        components: {
+            'header-component': Header,
+            'table-component': Table
+        }
+    }
+</script>
---
+++ src/components/pages/content/Header.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+    include ../../../tools/mixins.pug
+
+    +b.HEADER.header(ref="header")
+        +e.container.container
+            +e.search(:class="{'active': search.status}")
+                +e.input
+                    input(v-model="search.value")
+                    i(@click="search.status=!search.status")
+            +e.A.logo(href="#")
+                img(src="/public/media/logo.svg")
+            +e.BUTTON.button--hamburger.button--hamburger(
+                type="button"
+                @click="sidebar=!sidebar"
+                :class="{'active': sidebar}"
+                )
+                span
+                span
+                span
+            +e.wrap(:class="{'active': sidebar}")
+                +e.NAV.nav.nav__
+                    +e.list
+                        +e.A.item
+                            +e.I.img--bitcoin
+                            +e.A.link--(href="#")
+                            span Coins
+                        +e.A.item
+                            +e.I.img--reverse
+                            +e.A.link(href="#")
+                            span Exchanges
+                        +e.A.item--charts
+                            +e.A.link(href="#")
+                            span Charts
+                        +e.A.item
+                            +e.I.img--api
+                            span API
+                        +e.A.item--settings
+                            +e.I.img--settings
+                            span Settings
+
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                search: {
+                    status: false,
+                    value: ''
+                },
+                sidebar: false
+            }
+        },
+        methods: {
+
+        },
+        mounted() {
+            this.$emit('headerHeight', this.$refs.header.getBoundingClientRect().height)
+        },
+        destroyed() {
+
+        }
+    }
+</script>
